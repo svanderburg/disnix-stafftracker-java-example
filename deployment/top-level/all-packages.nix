40333de8154ea8aaaeac64aa74788bbecc558707
Sander van der Burg <svanderburg@gmail.com>
2011-01-27 13:34:06 +0000

    Remove ugly with pkgs;

--- deployment/top-level/all-packages.nix
+++ deployment/top-level/all-packages.nix
@@ -1,72 +1,73 @@
 {system, pkgs}:
 
-with pkgs;
-
 rec {
 ### Databases
 
   rooms = import ../pkgs/databases/rooms {
-    inherit stdenv;
+    inherit (pkgs) stdenv;
   };
   
   staff = import ../pkgs/databases/staff {
-    inherit stdenv;
+    inherit (pkgs) stdenv;
   };
   
   zipcodes = import ../pkgs/databases/zipcodes {
-    inherit stdenv;
+    inherit (pkgs) stdenv;
   };
 
 ### Web services + Clients
 
   GeolocationService = import ../pkgs/webservices/GeolocationService {
-    inherit stdenv apacheAnt axis2 geoipjava;
+    inherit (pkgs) stdenv apacheAnt axis2 geoipjava;
   };
 
   GeolocationServiceClient = import ../pkgs/webservices/GeolocationServiceClient {
-    inherit stdenv apacheAnt axis2 geoipjava;
+    inherit (pkgs) stdenv apacheAnt axis2 geoipjava;
   };
   
   RoomService = import ../pkgs/webservices/RoomService {
-    inherit stdenv apacheAnt axis2;
+    inherit (pkgs) stdenv apacheAnt axis2;
   };
   
   RoomServiceWrapper = import ../pkgs/webservices/RoomService/wrapper.nix {
-    inherit stdenv RoomService;
+    inherit (pkgs) stdenv;
+    inherit RoomService;
   };
   
   RoomServiceClient = import ../pkgs/webservices/RoomServiceClient {
-    inherit stdenv apacheAnt axis2;
+    inherit (pkgs) stdenv apacheAnt axis2;
   };
 
   StaffService = import ../pkgs/webservices/StaffService {
-    inherit stdenv apacheAnt axis2;
+    inherit (pkgs) stdenv apacheAnt axis2;
   };
   
   StaffServiceWrapper = import ../pkgs/webservices/StaffService/wrapper.nix {
-    inherit stdenv StaffService;
+    inherit (pkgs) stdenv;
+    inherit StaffService;
   };
   
   StaffServiceClient = import ../pkgs/webservices/StaffServiceClient {
-    inherit stdenv apacheAnt axis2;
+    inherit (pkgs) stdenv apacheAnt axis2;
   };
     
   ZipcodeService = import ../pkgs/webservices/ZipcodeService {
-    inherit stdenv apacheAnt axis2;
+    inherit (pkgs) stdenv apacheAnt axis2;
   };
   
   ZipcodeServiceWrapper = import ../pkgs/webservices/ZipcodeService/wrapper.nix {
-    inherit stdenv ZipcodeService;
+    inherit (pkgs) stdenv;
+    inherit ZipcodeService;
   };
   
   ZipcodeServiceClient = import ../pkgs/webservices/ZipcodeServiceClient {
-    inherit stdenv apacheAnt axis2;
+    inherit (pkgs) stdenv apacheAnt axis2;
   };
 
 ### Web applications
 
   StaffTracker = import ../pkgs/webapplications/StaffTracker {
-    inherit stdenv apacheAnt axis2;
+    inherit (pkgs) stdenv apacheAnt axis2;
     inherit GeolocationServiceClient RoomServiceClient StaffServiceClient ZipcodeServiceClient;
   };
 }
