1ffb9b3544649f9bbbbff08799ad6ea5a4fb4c8b
Sander van der Burg <svanderburg@gmail.com>
2011-05-16 20:26:03 +0000

    It seems that the codename of Firefox 4 is Tumucumaque, so the testcases won't work anymore. Fixed

--- release.nix
+++ release.nix
@@ -101,7 +101,7 @@ let
 	    # Start Firefox and take a screenshot
 	    
 	    $test3->mustSucceed("firefox http://test2:8080/StaffTracker &");
-	    $test3->waitForWindow(qr/Namoroka/);
+	    $test3->waitForWindow(qr/Tumucumaque/);
 	    $test3->mustSucceed("sleep 30");
 	    $test3->screenshot("screen");
 	  '';
