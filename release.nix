63f574b199bd1e599e92de287756ced52421be39
Sander van der Burg <svanderburg@gmail.com>
2019-09-01 23:06:37 +0200

    Fix services testcase to work with Firefox in the latest Nixpkgs

--- release.nix
+++ release.nix
@@ -6,12 +6,12 @@
 
 let
   pkgs = import nixpkgs {};
-  
+
   jobs = rec {
     tarball =
       let
         pkgs = import nixpkgs {};
-        
+
         disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
           inherit nixpkgs;
         };
@@ -22,7 +22,7 @@ let
         src = disnix_stafftracker_java_example;
         inherit officialRelease;
       };
-    
+
     build =
       pkgs.lib.genAttrs systems (system:
         let
@@ -38,7 +38,7 @@ let
           networkFile = "deployment/DistributedDeployment/network.nix";
           distributionFile = "deployment/DistributedDeployment/distribution.nix";
         });
-    
+
     tests = {
       services =
         let
@@ -55,31 +55,31 @@ let
             ''
               # Wait until the front-end application is deployed
               $test2->waitForFile("/var/tomcat/webapps/StaffTracker/stafftable.jsp");
-              
+
               # Wait a little longer and capture the output of the entry page
               my $result = $test3->mustSucceed("sleep 30; curl --fail http://test2:8080/StaffTracker/stafftable.jsp");
-              
+
               # The entry page should contain my name :-)
-              
+
               if ($result =~ /Sander/) {
                   print "Entry page contains Sander!\n";
               }
               else {
                   die "Entry page should contain Sander!\n";
               }
-              
+
               # Start Firefox and take a screenshot
-              
+
               $test3->mustSucceed("firefox http://test2:8080/StaffTracker &");
-              $test3->waitForWindow(qr/Nightly/);
+              $test3->waitForWindow(qr/Firefox/);
               $test3->mustSucceed("sleep 30");
               $test3->screenshot("screen");
             '';
         };
-    
+
       monolithic =
         with import "${nixpkgs}/nixos/lib/testing.nix" { system = builtins.currentSystem; };
-      
+
         simpleTest {
           nodes = {
             test1 = import ./deployment/configurations/test-vm-monolithic.nix;
@@ -87,22 +87,22 @@ let
           testScript = 
             ''
               startAll;
-              
+
               # Wait for the relevant system services to start
               $test1->waitForJob("mysql");
               $test1->waitForJob("tomcat");
-              
+
               # Deploy the mutable components part of the NixOS configuration
               $test1->mustSucceed("dysnomia-containers --deploy");
-              
+
               # Wait until the front-end application is deployed
               $test1->waitForFile("/var/tomcat/webapps/StaffTracker/stafftable.jsp");
-              
+
               # Wait a little longer and capture the output of the entry page
               my $result = $test1->mustSucceed("sleep 30; curl --fail http://localhost:8080/StaffTracker/stafftable.jsp");
-              
+
               # The entry page should contain my name :-)
-              
+
               if ($result =~ /Sander/) {
                   print "Entry page contains Sander!\n";
               } else {
@@ -110,13 +110,13 @@ let
               }
           '';
         };
-    
+
       containers =
         let
           disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
             inherit nixpkgs;
           };
-          
+
           manifest = disnixos.buildManifest {
             name = "disnix-stafftracker-java-containers-example";
             version = builtins.readFile ./version;
@@ -134,21 +134,21 @@ let
             ''
               $test2->waitForFile("/run/mysqld/mysqld.sock");
               $test2->mustSucceed("echo 'show databases;' | ${pkgs.mariadb}/bin/mysql --user=root --password=secret");
-              
+
               $test1->waitForFile("/var/tomcat/webapps");
               $test1->mustSucceed("sleep 10; curl http://localhost:8080");
-              
+
               $test2->waitForFile("/var/tomcat/webapps");
               $test2->mustSucceed("sleep 10; curl http://localhost:8080");
             '';
         };
-        
+
       multicontainers =
         let
           disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
             inherit nixpkgs;
           };
-          
+
           manifest = disnixos.buildManifest {
             name = "disnix-stafftracker-java-multicontainers-example";
             version = builtins.readFile ./version;
@@ -168,7 +168,7 @@ let
               $test2->mustSucceed("sleep 10; curl http://localhost:8080");
               $test2->waitForFile("/var/tomcat-test/webapps");
               $test2->mustSucceed("sleep 10; curl http://localhost:8081");
-              
+
               $test1->waitForFile("/run/mysqld-production/mysqld.sock");
               $test1->mustSucceed("echo 'show databases;' | ${pkgs.mariadb}/bin/mysql --socket=/run/mysqld-production/mysqld.sock --user=root --password=secret");
               $test1->waitForFile("/run/mysqld-test/mysqld.sock");
