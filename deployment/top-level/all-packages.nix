1b79b8dc95eea3bbc598081fd4bc9825fa4cb942
Sander van der Burg <svanderburg@gmail.com>
2020-04-27 22:17:31 +0200

    Remove whitespaces

--- deployment/top-level/all-packages.nix
+++ deployment/top-level/all-packages.nix
@@ -4,37 +4,37 @@ let
   callPackage = pkgs.lib.callPackageWith (pkgs // self);
 
   self = {
-    
+
 ### Databases
-    
+
     rooms = callPackage ../pkgs/databases/rooms { };
-    
+
     staff = callPackage ../pkgs/databases/staff { };
-    
+
     zipcodes = callPackage ../pkgs/databases/zipcodes { };
-    
+
 ### Web services + Clients
-    
+
     GeolocationService = callPackage ../pkgs/webservices/GeolocationService { };
 
     GeolocationServiceClient = callPackage ../pkgs/webservices/GeolocationServiceClient { };
-    
+
     RoomService = callPackage ../pkgs/webservices/RoomService { };
-    
+
     RoomServiceWrapper = callPackage ../pkgs/webservices/RoomService/wrapper.nix { };
-    
+
     RoomServiceClient = callPackage ../pkgs/webservices/RoomServiceClient { };
-    
+
     StaffService = callPackage ../pkgs/webservices/StaffService { };
-    
+
     StaffServiceWrapper = callPackage ../pkgs/webservices/StaffService/wrapper.nix { };
-    
+
     StaffServiceClient = callPackage ../pkgs/webservices/StaffServiceClient { };
-    
+
     ZipcodeService = callPackage ../pkgs/webservices/ZipcodeService { };
-    
+
     ZipcodeServiceWrapper = callPackage ../pkgs/webservices/ZipcodeService/wrapper.nix { };
-    
+
     ZipcodeServiceClient = callPackage ../pkgs/webservices/ZipcodeServiceClient { };
 
 ### Web applications
@@ -42,13 +42,13 @@ let
     StaffTracker = callPackage ../pkgs/webapplications/StaffTracker { };
 
 ### Containers
-    
+
     mysql-database = callPackage ../pkgs/containers/mysql-database {
       dysnomia = pkgs.dysnomia.override (origArgs: {
         enableMySQLDatabase = true;
       });
     };
-    
+
     tomcat-webapplication = callPackage ../pkgs/containers/tomcat-webapplication {
       tomcat = pkgs.tomcat8;
       commonLibs = [ "${pkgs.mysql_jdbc}/share/java/mysql-connector-java.jar" ];
@@ -58,7 +58,7 @@ let
     };
 
 ### Multi-containers
-    
+
     mysql-production = callPackage ../pkgs/containers/mysql-database {
       dysnomia = pkgs.dysnomia.override (origArgs: {
         enableMySQLDatabase = true;
@@ -70,7 +70,7 @@ let
       pidDir = "/run/mysqld-production";
       port = 3306;
     };
-    
+
     mysql-test = callPackage ../pkgs/containers/mysql-database {
       dysnomia = pkgs.dysnomia.override (origArgs: {
         enableMySQLDatabase = true;
@@ -82,7 +82,7 @@ let
       pidDir = "/run/mysqld-test";
       port = 3307;
     };
-    
+
     tomcat-production = callPackage ../pkgs/containers/tomcat-webapplication {
       tomcat = pkgs.tomcat8;
       commonLibs = [ "${pkgs.mysql_jdbc}/share/java/mysql-connector-java.jar" ];
@@ -98,7 +98,7 @@ let
       httpsPort = 8443;
       ajpPort = 8009;
     };
-    
+
     tomcat-test = callPackage ../pkgs/containers/tomcat-webapplication {
       tomcat = pkgs.tomcat8;
       commonLibs = [ "${pkgs.mysql_jdbc}/share/java/mysql-connector-java.jar" ];
